59c099780c2e4fa819895ca5c7d91066db8ac72a
Quantumdrill <quantumdrill317@gmail.com>
2026-02-21 05:16:34 -0500

    working popup colliders

--- popup_test/src/components/home.vue
+++ popup_test/src/components/home.vue
@@ -3,16 +3,21 @@ let popups = {}
 let browserBarHeight
 let scrn = {x: screen.availWidth, y:screen.availHeight}
 function testBrowserBarHeight (){
-    let testPopup = window.open("/","test","top=200,width=100,height=100")
+    let testPopup = window.open("/","test","top=0,left=0,width=400,height=400")
+    testPopup.resizeTo(400,400)
     browserBarHeight = testPopup.outerHeight-testPopup.innerHeight
+    console.log(testPopup.outerHeight,testPopup.innerHeight,testPopup.outerWidth,testPopup.innerWidth,browserBarHeight)
     testPopup.close()
 }
-function openPopup(name,left,top){
-    return window.open(
+function openPopup(name,left,top,size){
+    let newWindow
+    newWindow = window.open(
         `/${name}`, 
         `${name}`, 
-        `left=${left},top=${top},width=400,height=${400-browserBarHeight}`
+        `left=${left},top=${top},width=500,height=500`
     )
+    newWindow.resizeTo(size,size)
+    return newWindow
 }
 function moveDown(){
     popups.p1.moveBy(0, 100)
@@ -22,6 +27,8 @@ function moveDown(){
 function colliderInitialize(){
     popups.p3.xp = popups.p3.screenX
     popups.p3.yp = popups.p3.screenY
+    popups.p4.vx = 0
+    popups.p4.vy = 0
 }
 function colliderTick(){
     colliderGetCurrentVelocity()
@@ -31,23 +38,45 @@ function colliderTick(){
     requestAnimationFrame(colliderTick)
 }
 function collisionInter(){
-    if (Math.abs(popups.p4.screenX-popups.p3.screenX)<400&&Math.abs(popups.p4.screenY-popups.p3.screenY)<400){
-        console.log("co")
-        popups.p4.vx = popups.p3.vx
-        popups.p4.vy = popups.p3.vy
+    if (Math.abs(popups.p4.screenX-popups.p3.screenX)<400&&Math.abs(popups.p4.screenY-popups.p3.screenY)<400){ //detect for collision with driver popup
+        if (Math.abs(popups.p4.screenX-popups.p3.screenX)>Math.abs(popups.p4.screenY-popups.p3.screenY)){ //detect x or y collision, > meaning x
+            if (popups.p4.screenX>popups.p3.screenX){ // p4 to the right of p3
+                popups.p4.moveTo(popups.p3.screenX+400,popups.p4.screenY)
+                popups.p4.vx*=-1
+            } else if (popups.p4.screenX<popups.p3.screenX){ // p4 to the left of p3
+                popups.p4.moveTo(popups.p3.screenX-400,popups.p4.screenY)
+                popups.p4.vx*=-1
+            }   
+        } else {
+            if (popups.p4.screenY>popups.p3.screenY){ // p4 to the bottom of p3
+                popups.p4.moveTo(popups.p4.screenX,popups.p3.screenY+400)
+                popups.p4.vy*=-1
+            } else if (popups.p4.screenY<popups.p3.screenY){ // p4 to the top of p3
+                popups.p4.moveTo(popups.p4.screenX,popups.p3.screenY-400)
+                popups.p4.vy*=-1
+            }
+        }
+         
+        popups.p4.vx += popups.p3.vx
+        popups.p4.vy += popups.p3.vy
     }
 }
 function collisionEdge(){
-    if (popups.p4.screenX<5||popups.p4.screenX+400>scrn.x-40){
-        
+    if (popups.p4.screenX<5||popups.p4.screenX+400>scrn.x-5){
         popups.p4.vx*=-1
-    } else if (popups.p4.screenY<5||popups.p4.screenY+400>scrn.y-40){
+    } else if (popups.p4.screenY<5||popups.p4.screenY+400>scrn.y-5){
         popups.p4.vy*=-1
     }
 }
 function colliderUpdate(){
+    if (popups.p4.vx>20){  // speed limit
+        popups.p4.vx=20
+    }
+    if (popups.p4.vy>20){
+        popups.p4.vy=20
+    }
     popups.p4.moveBy(popups.p4.vx,popups.p4.vy)
-    popups.p4.vx*=0.99
+    popups.p4.vx*=0.99 // speed damping
     popups.p4.vy*=0.99
 }
 function colliderGetCurrentVelocity(){
@@ -58,9 +87,9 @@ function colliderGetCurrentVelocity(){
 }
 </script>
 <template>
-    <button @click="testBrowserBarHeight();popups.p1 = openPopup('popup1',100,100);popups.p2 = openPopup('popup2',600,100)" >popups</button>
-    <button @click="testBrowserBarHeight();popups.p3 = openPopup('popup3',100,100);popups.p4 = openPopup('popup4',600,100);requestAnimationFrame(colliderTick())" >colliders</button>
+    <button @click="popups.p1 = openPopup('popup1',100,100,400);popups.p2 = openPopup('popup2',600,100,400)" >popups</button>
     <button @click="moveDown">move both down</button>
+    <button @click="popups.p3 = openPopup('popup3',100,100,400);popups.p4 = openPopup('popup4',600,100,400);colliderInitialize();requestAnimationFrame(colliderTick())" >colliders</button>
 </template>
 <style scoped>
 </style>
