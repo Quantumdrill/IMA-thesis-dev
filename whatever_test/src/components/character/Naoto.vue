4b59c3695b705a46d95a9ee579ed1a600b2ad6cd
Quantumdrill <quantumdrill317@gmail.com>
2026-04-15 03:19:39 -0400

    animation transition, character movement and global 8fps sync

--- whatever_test/src/components/character/Naoto.vue
+++ whatever_test/src/components/character/Naoto.vue
@@ -5,66 +5,98 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
 import {gsap} from "gsap"
 import {onMounted, useTemplateRef} from "vue"
 import { LoadingManager } from "three"
-import { loadCharAnim, loadCharSkm } from "../../functions/char"
+import { loadCharAnim, loadCharSkm, animTransition, charMove } from "../../functions/char"
 
 const scene = new THREE.Scene()
 const canvas = useTemplateRef("canvasDom")
 let renderer
-const camZoomFactor = 0.03
-const cam = new THREE.OrthographicCamera(-1/camZoomFactor,1/camZoomFactor,1/camZoomFactor,-1/camZoomFactor,0.01,1000)
-cam.position.set(50,20,0)
-cam.rotation.y = Math.PI/2
-const topLight = new THREE.DirectionalLight(0xffffff, 1)
-topLight.position.set(0, 100, 0)
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
-scene.add(cam,topLight,ambientLight)
+const scrnRatio = window.innerWidth/window.innerHeight
+
+//fps
+const fps = 8
+let accumulatedDelta = 0
+const clock = new THREE.Timer()
+
+//camera
+const camZoomFactor = 5.5 
+const cam = new THREE.OrthographicCamera(-window.innerWidth/camZoomFactor,window.innerWidth/camZoomFactor,window.innerHeight/camZoomFactor,-window.innerHeight/camZoomFactor,0.01,1000)
+cam.position.set(0,25,50)
+
+//lights
+const backLightFront = new THREE.DirectionalLight(0xffffff, 2)
+backLightFront.position.set(50, 20, -50)
+const backLightBack = new THREE.DirectionalLight(0xffffff, 1)
+backLightBack.position.set(-50, 10, -50)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+scene.add(cam,backLightFront,backLightBack,ambientLight)
+
+//loader
 const loadingManager = new LoadingManager()
 const modelLoader = new FBXLoader(loadingManager)
 
+//characters
 const naoto = {
     mesh: null,
     skm: null,
     animClips: {
-        run: null
     },
     animActions: {
-        run: null
     },
-    mixer: null
+    mixer: null,
+    currentAnim: "idle",
 }
+const unitToVw = 50/8 // height of naoto is 8vw, and 50 units, so 50/8 = 6.25 units per vw
+
+
 
 onMounted(() => {
-    renderer = new THREE.WebGLRenderer({antialias:true,canvas:canvas.value})
-    renderer.setSize(window.innerWidth/2, window.innerWidth/2)
+    renderer = new THREE.WebGLRenderer({antialias:true,canvas:canvas.value,alpha: true})
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio(window.devicePixelRatio)
     
     loadCharSkm("Naoto", naoto, modelLoader)
     loadCharAnim("Naoto", "run", naoto, modelLoader)
+    loadCharAnim("Naoto", "idle", naoto, modelLoader)
 
     loadingManager.onLoad = () => {
-        console.log(naoto)
-        naoto.skm.material = new THREE.MeshStandardMaterial({color: 0xffffff})
+        naoto.skm.material = new THREE.MeshLambertMaterial({color: 0xffffff})
         scene.add(naoto.mesh)
+        naoto.mesh.rotation.y = Math.PI/2
+        naoto.mesh.position.set(-45*unitToVw,-41*unitToVw/scrnRatio,0)
 
         naoto.mixer = new THREE.AnimationMixer(naoto.mesh)
         naoto.animActions.run = naoto.mixer.clipAction(naoto.animClips.run)
+        naoto.animActions.idle = naoto.mixer.clipAction(naoto.animClips.idle)
 
-        naoto.animActions.run.play()
+        naoto.animActions.idle.play()
+        window.addEventListener("click", (e) => {
+            charMove(naoto, 80, 0)
+            animTransition(naoto, naoto.currentAnim, "run")
+        })
         animTick()
     }
 })
 
-function animTick(){
-    naoto.mixer.update(1/60)
-
+function globalUpdatePerFrame(){
+    naoto.mixer.update(1/fps)
     renderer.render(scene,cam)
+}
+
+function animTick(){
+    clock.update()
+    let delta = clock.getDelta()
+    accumulatedDelta += delta
+    if (accumulatedDelta >= 1/fps){ //enable 8fps for all anim clips and transitions
+        accumulatedDelta = 0
+        globalUpdatePerFrame()
+    }
+
     requestAnimationFrame(animTick)
 }
 
 </script>
 <template>
-    <div id="canvasContainer">
-        <canvas id="canvas" ref="canvasDom"></canvas>
-    </div>
+    <canvas id="canvas" ref="canvasDom"></canvas>
 </template>
 <style scoped>
 
@@ -78,8 +110,8 @@ function animTick(){
 }
 
 #canvas{
-    width: 10vw;
-    height: 10vw;
+    width: 100vw;
+    height: 100vh;
     position: fixed;
 }
 
--- whatever_test/src/functions/char.js
+++ whatever_test/src/functions/char.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import gsap from "gsap"
 
 export function loadCharAnim(charname, animName, charObj, modelLoader){
     modelLoader.load(`/src/assets/char/${charname}_anim_${animName}.fbx`, (loaded) => {
@@ -12,6 +13,30 @@ export function loadCharSkm(charname, charObj, modelLoader){
     modelLoader.load(`/src/assets/char/${charname}_skm.fbx`, (loaded) => {
         charObj.mesh = loaded
         charObj.skm = loaded.children[0]
-        console.log(charObj)
+    })
+}
+
+export function animTransition(charObj, currentAnimName, newAnimName, duration=0.25){
+    let currentAnim = charObj.animActions[currentAnimName]
+    let newAnim = charObj.animActions[newAnimName]
+    newAnim.reset()
+    newAnim.play()
+    currentAnim.crossFadeTo(newAnim, duration)
+    setTimeout(() => {
+        charObj.currentAnim = newAnimName
+    }, duration*1000)
+}
+
+export function charMove(charObj, displacementX, displacementY, parabola=false){
+    let speed = 24
+    let realDisplacementX = displacementX*50/8
+    let realDisplacementY = displacementY*50/8
+    let duration = displacementX/speed
+    let frames = Math.floor(duration/0.125)
+    gsap.to(charObj.mesh.position, {
+        x: `+=${realDisplacementX}`,
+        y: `-=${realDisplacementY}`,
+        duration: duration,
+        ease: "none"
     })
 }
