5bdab4b050cdb75062c581054c115cb09c9b8915
Quantumdrill <quantumdrill317@gmail.com>
2026-05-05 19:11:13 -0400

    cleanup 1

--- whatever_test/src/components/stages/stage21.vue
+++ whatever_test/src/components/stages/stage21.vue
@@ -5,7 +5,8 @@ import { popupNewInstance, popupFixSize, popupFixPosition, popupSnapCheck, popup
 import Naoto from "../character/Naoto.vue"
 
 let router = useRouter()
-let lorumPlaceholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+
+console.log(window.screenY)
 const browserTopHeight = window.outerHeight - window.innerHeight
 const bridges = {
     left: {
